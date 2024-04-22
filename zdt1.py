6d4b10f794a824d855ea121cde04a571a8b1a467
RyotaKato0811 <167410258+RyotaKato0811@users.noreply.github.com>
2024-04-22 16:40:14 +0900

    Create zdt1.py

---
+++ zdt1.py
@@ -0,0 +1,34 @@
+import math
+import optuna
+def f1(X):
+    return X[0]
+
+def f2(X):
+    g = 1 + 9 * sum(X[1:]) / (len(X)-1)
+    h = 1 - math.sqrt(X[0]/g)
+　   return g*h
+def objective(trial):
+    X = [trial.suggest_float(f"x{i}",0,1)for i in range(30)]
+    v1 = f1(X)
+    v2 = f2(X)
+    # f1<0.5
+    trial.set_user_attr("constraints",[v1-0.5])
+    return v1,v2
+
+sampler = optuna.samplers.NSGAIISampler(
+    crossover = optuna.samplers.nsgaii.UNDXCrossover(),
+    #
+    constraints_func=lambda trial: trial.user_attrs["constraints"]
+)
+
+study = optuna.create_study(
+    directions = ["minimize","minimize"],
+    sampler = sampler
+)
+
+study.optimize(objective, n_trials=1000)
+
+optuna.visualization.plot_pareto_front(
+    study,
+    include_dominated_trials=True
+).show()
